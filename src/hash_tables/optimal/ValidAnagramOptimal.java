9a185e127d2554ce09774cac8c51e87b616e8f23
iamkhuzaim <khuzaim.dev@gmail.com>
2026-06-28 12:52:18 +0500

    feat(hash-tables): add optimal solution for Valid Anagram

--- src/hash_tables/optimal/ValidAnagramOptimal.java
+++ src/hash_tables/optimal/ValidAnagramOptimal.java
@@ -1,5 +1,48 @@
 package hash_tables.optimal;
 
+/*
+Problem: Valid Anagram
+
+Optimal Approach:
+Count the frequency of each character
+using an array.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
 public class ValidAnagramOptimal {
-    
+
+    public static boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int num : count) {
+
+            if (num != 0) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "anagram";
+        String t = "nagaram";
+
+        System.out.println(isAnagram(s, t));
+    }
 }
+
