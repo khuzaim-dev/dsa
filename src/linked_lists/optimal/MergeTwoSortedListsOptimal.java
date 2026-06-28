82c28019e6283e1aa8f40256fea7bf31816b32a9
iamkhuzaim <khuzaim.dev@gmail.com>
2026-06-28 17:41:20 +0500

    feat(linked-lists): add optimal solution for Merge Two Sorted Lists

--- src/linked_lists/optimal/MergeTwoSortedListsOptimal.java
+++ src/linked_lists/optimal/MergeTwoSortedListsOptimal.java
@@ -1,5 +1,68 @@
 package linked_lists.optimal;
 
+/*
+Problem: Merge Two Sorted Lists
+
+Optimal Approach:
+Merge both lists using two pointers.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
 public class MergeTwoSortedListsOptimal {
-    
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        }
+
+        if (list2 != null) {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode merged = mergeTwoLists(list1, list2);
+
+        printList(merged);
+    }
 }
