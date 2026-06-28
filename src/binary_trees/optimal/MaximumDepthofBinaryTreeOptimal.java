4d2d6acbb51f7196bad4aaf7994c5dd7ba186653
iamkhuzaim <khuzaim.dev@gmail.com>
2026-06-28 17:22:28 +0500

    feat(binary-trees): add optimal solution for Maximum Depth of Binary Tree

--- src/binary_trees/optimal/MaximumDepthofBinaryTreeOptimal.java
+++ src/binary_trees/optimal/MaximumDepthofBinaryTreeOptimal.java
@@ -1,5 +1,34 @@
 package binary_trees.optimal;
 
-public class MaximumDepthofBinaryTreeOptimal {
-    
+/*
+Problem: Maximum Depth of Binary Tree
+
+Optimal Approach:
+Use recursive DFS.
+
+Time Complexity: O(n)
+Space Complexity: O(h)
+*/
+
+public class MaximumDepthOfBinaryTreeOptimal {
+
+    public static int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(maxDepth(root));
+    }
 }
