acdd69c02c5107cb1f82aac6d084af202d4ee359
iamkhuzaim <khuzaim.dev@gmail.com>
2026-06-28 17:14:05 +0500

    feat(two-pointers): add optimal solution for Reverse String

--- src/two_pointers/optimal/ReverseStringOptimal.java
+++ src/two_pointers/optimal/ReverseStringOptimal.java
@@ -1,5 +1,40 @@
 package two_pointers.optimal;
 
+/*
+Problem: Reverse String
+
+Optimal Approach:
+Use two pointers and swap characters
+from both ends.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
 public class ReverseStringOptimal {
-    
+
+    public static void reverseString(char[] s) {
+
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+
+        reverseString(s);
+
+        System.out.println(s);
+    }
 }
