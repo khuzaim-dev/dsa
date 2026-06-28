d812a25a06cad0ff7cd4d14d3f61c2d455f84af0
iamkhuzaim <khuzaim.dev@gmail.com>
2026-06-28 17:44:52 +0500

    feat(heaps): add optimal solution for Kth Largest Element in an Array

--- src/heaps/optimal/KthLargestElementInArrayOptimal.java
+++ src/heaps/optimal/KthLargestElementInArrayOptimal.java
@@ -1,5 +1,41 @@
 package heaps.optimal;
 
+import java.util.PriorityQueue;
+
+/*
+Problem: Kth Largest Element in an Array
+
+Optimal Approach:
+Maintain a min-heap of size k.
+The heap root is always the kth largest element.
+
+Time Complexity: O(n log k)
+Space Complexity: O(k)
+*/
+
 public class KthLargestElementInArrayOptimal {
-    
+
+    public static int findKthLargest(int[] nums, int k) {
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+
+            minHeap.offer(num);
+
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+
+        System.out.println(findKthLargest(nums, k));
+    }
 }
