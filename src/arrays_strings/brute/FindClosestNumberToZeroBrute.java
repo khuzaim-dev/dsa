c516dcfdb84573e9606a73ce0d1a015752bf03aa
iamkhuzaim <khuzaim.dev@gmail.com>
2026-06-28 09:48:36 +0500

    chore: initialize DSA repository structure

---
+++ src/arrays_strings/brute/FindClosestNumberToZeroBrute.java
@@ -0,0 +1,13 @@
+
+package arrays_strings.brute;
+
+public class FindClosestNumberToZeroBrute {
+
+    public static void main(String[] args) {
+        
+
+        System.out.println("Hello world i am here");
+    }
+
+    
+}
